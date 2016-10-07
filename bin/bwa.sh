5b5af99fefcad71c19cedac494ba834dcac5cd5a
bowhan <bowhan@icloud.com>
2016-10-07 08:49:45 -0400

    fix a bug naming bwa bam output

--- bin/bwa.sh
+++ bin/bwa.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 if [[ $# -lt 2 ]]; then
-	echo "usage: $0 input.fa index_name threads=8"
+	echo "usage: $0 input.fa index threads=8"
 	exit 1
 fi
 
 inputfile=${1}
 inputname=$(basename ${inputfile})
-index_name=${2}
+index=${2}
+index_name=$(basename $index)
 threads=${3:-8}
 
 function _bwaIndexCheck {
@@ -20,15 +21,15 @@ function _bwaIndexCheck {
     return 0;
 }
 	
-if ! _bwaIndexCheck $index_name; then
-	echo "failed to detect bwa index at $index_name"
+if ! _bwaIndexCheck $index; then
+	echo "failed to detect bwa index at $index"
 	exit 1
 fi
 
 bwa mem \
     -t $threads \
     -k17 -W40 -r10 -A1 -B1 -O1 -E1 -L0 \
-    ${index_name} \
+    ${index} \
     ${inputfile} \
 | samtools view -bS - \
 | samtools sort -f - bam/${inputname%f[aq]*}${index_name}.sorted.bam \
